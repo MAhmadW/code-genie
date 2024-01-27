a702d9838a4e7e142af3500212f332db14de12d5
Ahmad Waseem <ahmadwaseem.professional@gmail.com>
2024-01-27 17:39:05 +0500

    Code Generation for JS and Python

---
+++ genie.py
@@ -0,0 +1,65 @@
+import sys
+import os
+
+import openai
+
+from prompts.basic import BASIC_PROMPT
+from env import OPENAI_API_KEY
+
+SOLUTIONS_PATH = './solutions'
+
+OPENAI_CLIENT = openai.OpenAI(api_key=OPENAI_API_KEY)
+
+LANG_MAP = {'python':'py','javascript':'js'}
+
+MODEL = 'gpt-4'
+
+def num_args():
+    num_args = len(sys.argv)-1
+    return num_args
+
+def get_arg(num):
+    return sys.argv[num]
+
+def build_prompt(question,lang):
+    prompt = f' The question is \n {question}.\n'+ f' The programming language of the solution needs to be {lang}.'
+    return prompt
+
+def generate_response(user_message):
+    response = OPENAI_CLIENT.chat.completions.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": BASIC_PROMPT},
+        {"role": "user", "content": user_message}
+    ]
+    )
+
+    return response.choices[0].message.content
+
+def main():
+    try:
+        os.mkdir(SOLUTIONS_PATH)
+    except:
+        print(f'Solutions directory already exists as {SOLUTIONS_PATH}')
+
+    if num_args() == 2:
+        q_file = get_arg(1)
+
+        lang = get_arg(2)
+
+        q = ''
+
+        with open(q_file,'r') as f:
+            q = f.read()
+
+        script = generate_response(build_prompt(q, lang))
+
+        with open(os.path.join(SOLUTIONS_PATH,f'script_file.{LANG_MAP[lang]}'),'w') as f:
+            f.write(script)
+
+        
+
+
+
+if __name__=='__main__':
+    main()
